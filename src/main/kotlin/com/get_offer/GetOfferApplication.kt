228c6b5298942d787a204df7e1d2e7d13ebca7ae
pine_lee <dlthf555@gmail.com>
2024-09-23 22:04:52 +0900

    initial commit

---
+++ src/main/kotlin/com/get_offer/GetOfferApplication.kt
@@ -0,0 +1,11 @@
+package com.get_offer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GetOfferApplication
+
+fun main(args: Array<String>) {
+	runApplication<GetOfferApplication>(*args)
+}
---
+++ src/test/kotlin/com/get_offer/GetOfferApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.get_offer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GetOfferApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
