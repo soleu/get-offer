331b42178eee11190ba77c1673c04e1b1a703aab
pine_lee <dlthf555@gmail.com>
2024-10-01 22:57:30 +0900

    refactor : test data 테스트용으로만 수정

--- src/test/kotlin/com/get_offer/product/controller/ProductIntegrationTest.kt
+++ src/test/kotlin/com/get_offer/product/controller/ProductIntegrationTest.kt
@@ -4,13 +4,16 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
+
 @SpringBootTest
+@Sql("classpath:test_data.sql")
 @AutoConfigureMockMvc
 class ProductIntegrationTest(
     @Autowired val mockMvc: MockMvc,
--- src/test/resources/test_data.sql
+++ src/test/resources/test_data.sql
@@ -1,3 +1,6 @@
+TRUNCATE table USERS;
+TRUNCATE table PRODUCTS;
+
 INSERT INTO USERS (ID, NICKNAME, IMAGE, CREATED_AT, UPDATED_AT)
 VALUES (1, 'test', 'https://drive.google.com/file/d/1R9EIOoEWWgPUhY6e-t4VFuqMgknl7rm8/view?usp=sharing',
         '2024-01-01 00:00:00', '2024-01-01 00:00:00');
@@ -11,4 +14,3 @@ INSERT INTO PRODUCTS (ID, WRITER_ID, NAME, DESCRIPTION, CATEGORY, CURRENT_PRICE,
 VALUES (1, 1, 'nintendo', '닌텐도 새 제품', 'GAMES', 10000, 5000, 'IN_PROGRESS', '2024-01-02 00:00:00', '2024-01-04 00:00:00',
         '2024-01-02 00:00:00', '2024-01-02 00:00:00',
         '{"images":["https://picsum.photos/200/300","https://picsum.photos/200/300"]}');
-
