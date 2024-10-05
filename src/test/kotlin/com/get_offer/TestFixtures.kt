c876e7d3875f0053e052982225f4a76c3326d78d
pine_lee <dlthf555@gmail.com>
2024-10-05 23:49:15 +0900

    feat : 테스트 코드 정리 #11

---
+++ src/test/kotlin/com/get_offer/TestFixtures.kt
@@ -0,0 +1,52 @@
+package com.get_offer
+
+import com.get_offer.auction.domain.AuctionResult
+import com.get_offer.auction.domain.AuctionStatus
+import com.get_offer.product.domain.Category
+import com.get_offer.product.domain.Product
+import com.get_offer.product.domain.ProductImagesVo
+import com.get_offer.product.domain.ProductStatus
+import java.time.LocalDateTime
+
+object TestFixtures {
+    fun createProductInprogress(userId: Long?): Product {
+        return Product(
+            userId ?: 1L,
+            "product",
+            Category.GAMES,
+            ProductImagesVo(listOf("https://image1.png", "https://image2.png")),
+            "desc for product",
+            10000,
+            15000,
+            ProductStatus.IN_PROGRESS,
+            LocalDateTime.now(),
+            LocalDateTime.now()
+        )
+    }
+
+    fun createProductWait(userId: Long?): Product {
+        return Product(
+            userId ?: 2L,
+            "product2",
+            Category.SPORTS,
+            ProductImagesVo(listOf("https://image1.png", "https://image2.png")),
+            "desc for product",
+            12000,
+            15500,
+            ProductStatus.WAIT,
+            LocalDateTime.now(),
+            LocalDateTime.now()
+        )
+    }
+
+    fun createAuction(buyerId: Long?): AuctionResult {
+        return AuctionResult(
+            productId = 1L,
+            buyerId = buyerId ?: 1L,
+            finalPrice = 15500,
+            auctionStatus = AuctionStatus.COMPLETED,
+        )
+    }
+
+
+}
---
+++ src/test/kotlin/com/get_offer/auction/controller/AuctionControllerTest.kt
@@ -0,0 +1,5 @@
+package com.get_offer.auction.controller
+
+import org.junit.jupiter.api.Assertions.*
+
+class AuctionControllerTest
