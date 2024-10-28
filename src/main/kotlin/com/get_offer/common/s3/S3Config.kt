0c8fbeea5b516cbfdafb504ad766759aeecaa646
pine_lee <dlthf555@gmail.com>
2024-10-28 22:49:04 +0900

    feat : S3 버전 업

--- src/main/kotlin/com/get_offer/common/s3/S3Config.kt
+++ src/main/kotlin/com/get_offer/common/s3/S3Config.kt
@@ -1,13 +1,13 @@
 package com.get_offer.common.s3
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider
-import com.amazonaws.auth.BasicAWSCredentials
-import com.amazonaws.regions.Regions
-import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
+
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
 
 @Configuration
 class S3Config(
@@ -17,12 +17,10 @@ class S3Config(
     private val secretKey: String,
 ) {
     @Bean
-    fun amazonS3Client(): AmazonS3 {
-        val credentials = BasicAWSCredentials(accessKey, secretKey)
-        return AmazonS3ClientBuilder
-            .standard()
-            .withCredentials(AWSStaticCredentialsProvider(credentials))
-            .withRegion(Regions.AP_NORTHEAST_2)
+    fun amazonS3Client(): S3Client {
+        val credentials = AwsBasicCredentials.create(accessKey, secretKey)
+        return S3Client.builder().region(Region.AP_NORTHEAST_2)
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
             .build()
     }
 }
--- src/main/kotlin/com/get_offer/common/s3/S3FileManagement.kt
+++ src/main/kotlin/com/get_offer/common/s3/S3FileManagement.kt
@@ -1,18 +1,20 @@
 package com.get_offer.common.s3
 
-import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.model.ObjectMetadata
 import com.get_offer.multipart.FileValidate
 import java.util.*
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
+import software.amazon.awssdk.core.sync.RequestBody
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
 
 @Component
 class S3FileManagement(
     @Value("\${aws.s3.bucket}")
     private val bucket: String,
-    private val amazonS3: AmazonS3,
+    private val amazonS3: S3Client,
 ) {
     companion object {
         const val TYPE_IMAGE = "image"
@@ -27,17 +29,16 @@ class S3FileManagement(
             ?: throw IllegalStateException()
         FileValidate.checkImageFormat(originalFilename)
         val fileName = "${UUID.randomUUID()}-${originalFilename}"
-        val objectMetadata = setFileDateOption(
-            type = TYPE_IMAGE,
-            file = getFileExtension(originalFilename),
-            multipartFile = multipartFile
-        )
-        amazonS3.putObject(bucket, fileName, multipartFile.inputStream, objectMetadata)
-        return fileName
-    }
 
-    fun getFile(fileName: String): String {
-        return amazonS3.getUrl(bucket, fileName).toString()
+        val putObjectRequest = PutObjectRequest.builder()
+            .bucket(bucket)
+            .key(fileName)
+            .contentType("/${TYPE_IMAGE}/${getFileExtension(getFileExtension(originalFilename))}")
+            .contentLength(multipartFile.inputStream.available().toLong())
+            .build()
+
+        amazonS3.putObject(putObjectRequest, RequestBody.fromBytes(multipartFile.bytes))
+        return fileName
     }
 
     fun delete(fileNames: List<String>) {
@@ -45,22 +46,15 @@ class S3FileManagement(
     }
 
     fun delete(fileName: String) {
-        amazonS3.deleteObject(bucket, fileName)
+        val deleteObjectRequest = DeleteObjectRequest.builder()
+            .bucket(bucket)
+            .key(fileName)
+            .build()
+        amazonS3.deleteObject(deleteObjectRequest)
     }
 
     private fun getFileExtension(fileName: String): String {
         val extensionIndex = fileName.lastIndexOf('.')
         return fileName.substring(extensionIndex + 1)
-    }
-
-    private fun setFileDateOption(
-        type: String,
-        file: String,
-        multipartFile: MultipartFile
-    ): ObjectMetadata {
-        val objectMetadata = ObjectMetadata()
-        objectMetadata.contentType = "/${type}/${getFileExtension(file)}"
-        objectMetadata.contentLength = multipartFile.inputStream.available().toLong()
-        return objectMetadata
     }
 }
