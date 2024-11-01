386407e40d47b55f9f533e42850209d155effb1d
pine_lee <dlthf555@gmail.com>
2024-11-01 12:48:49 +0900

    feat : 경매 시작 스프링 배치 구현 #17

--- src/main/kotlin/com/get_offer/GetOfferApplication.kt
+++ src/main/kotlin/com/get_offer/GetOfferApplication.kt
@@ -1,9 +1,11 @@
 package com.get_offer
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableBatchProcessing
 class GetOfferApplication
 
 fun main(args: Array<String>) {
